82a3a87ad76ef23d1f0c5b04a2004e617fd2c6b8
Viktor Shuntikov <schuntikov.viktor@yandex.ru>
2025-08-25 16:41:06 +0700

    добавил отображение месяца

--- newYearTimer.js
+++ newYearTimer.js
@@ -1,3 +1,4 @@
+const monthEl = document.querySelector('[data-js-timer="months"]');
 const daysEl = document.querySelector('[data-js-timer="days"]');
 const hoursEl = document.querySelector('[data-js-timer="hours"]');
 const minutesEl = document.querySelector('[data-js-timer="minutes"]');
@@ -11,14 +12,22 @@ function toTheNewYear(time) {
 
   const interval = setInterval(() => {
     const now = new Date();
+
       const calcDates = theEndOfThisYear - now;
 
       let totalSeconds = Math.floor(calcDates / 1000);
+      let months = (theEndOfThisYear.getFullYear() - now.getFullYear()) * 12 +
+        (theEndOfThisYear.getMonth() - now.getMonth());
+
+      if (theEndOfThisYear.getDate() < now.getDate()) {
+        months -= 1;
+      }
       let days = Math.floor(totalSeconds / 86400);
       let hours = Math.floor((totalSeconds % 86400) / 3600);
       let minutes = Math.floor((totalSeconds % 3600) / 60);
       let seconds = totalSeconds % 60;
 
+      monthEl.textContent = months.toString().padStart(2, '0');
       daysEl.textContent = days;
       hoursEl.textContent = hours.toString().padStart(2, '0');
       minutesEl.textContent = minutes.toString().padStart(2, '0');
